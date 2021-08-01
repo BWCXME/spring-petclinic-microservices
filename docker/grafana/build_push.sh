675c7b25d19b1bda164fef2fd88bbdbbbe540936
Robin Long <binlon@amazon.com>
2021-08-01 11:22:28 +0800

    fix build command issue

--- docker/grafana/build_push.sh
+++ docker/grafana/build_push.sh
@@ -2,7 +2,7 @@
 aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin $ACCOUNT_ID.dkr.ecr.us-east-1.amazonaws.com
 
 # Build
-docker build -t $ACCOUNT_ID.dkr.ecr.us-east-1.amazonaws.com/spring-petclinic-msa/grafana-server:latest
+docker build -t $ACCOUNT_ID.dkr.ecr.us-east-1.amazonaws.com/spring-petclinic-msa/grafana-server:latest .
 
 # Push
 docker push $ACCOUNT_ID.dkr.ecr.us-east-1.amazonaws.com/spring-petclinic-msa/grafana-server:latest
