4e8067ca6e60ce23ae63fcde12d0d725da5be0f6
Robin Long <binlon@amazon.com>
2021-08-01 09:41:06 +0800

    add create_ecr_repos

---
+++ create_ecr_repos.sh
@@ -0,0 +1,7 @@
+aws ecr create-repository --repository-name spring-petclinic-msa/api-gateway
+aws ecr create-repository --repository-name spring-petclinic-msa/discovery-server
+aws ecr create-repository --repository-name spring-petclinic-msa/config-server
+aws ecr create-repository --repository-name spring-petclinic-msa/visits-service
+aws ecr create-repository --repository-name spring-petclinic-msa/vets-service
+aws ecr create-repository --repository-name spring-petclinic-msa/customers-service
+aws ecr create-repository --repository-name spring-petclinic-msa/admin-server
